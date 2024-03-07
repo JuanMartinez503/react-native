1802ca969e4a611881bc779eb1ee35f0866b63d4
Juan Martinez <juanmartinez5678@hotmail.com>
2024-03-07 15:11:16 -0500

    Update Home component to include SafeAreaView, ScrollView, and new components

--- app/index.js
+++ app/index.js
@@ -1,10 +1,31 @@
-import {View, Text} from 'react-native';
-
+import {View, ScrollView , SafeAreaView} from 'react-native'; //safe area view is used to make sure the content is not hidden by the notch or the home indicator
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import {COLORS, icons, images, SIZES} from '../constants'
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-        <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite , }}>
+        <Stack.Screen options={{
+            headerStyle: {
+                backgroundColor: COLORS.lightWhite,
+            
+            },
+            headerShadowVisible: false,
+            headerLeft: ()=> (<ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />),
+            headerRight: ()=> (<ScreenHeaderBtn iconUrl={icons.profile} dimension='60%' />),
+            headerTitle: ''
+        }} />
+        <ScrollView >
+            <View style={{flex:1, padding:SIZES.medium}} >
+                <Welcome />
+                <Nearbyjobs />
+                <Popularjobs />
+            </View>
+   
+        </ScrollView>
+        </SafeAreaView>
     );
     }
 export default Home;
